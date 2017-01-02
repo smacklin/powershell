13b49aaded2c203cf9e07c658da7794cb213b0f9
Simon Macklin <simon.macklin@icloud.com>
2017-01-02 13:15:19 +0000

    added desciption

--- Update-WebConfigurtation/Update-WebConfiguration.psm1
+++ Update-WebConfigurtation/Update-WebConfiguration.psm1
@@ -1,9 +1,10 @@
 ﻿function Update-WebConfiguration {
 <#
 .Synopsis
-   
+   Updates the IIS app settings section of the web.config file
 .DESCRIPTION
-   
+   Updates the appsettings section of a IIS web config file.  Can be used to dyanicaml change settings like 
+   the sql connection string.
 .EXAMPLE
    
 .EXAMPLE
